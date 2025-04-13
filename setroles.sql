2c9217c58b650e947b289efac8ac448514f3e0af
Melanie Chebet <chebetmelanie4@gmail.com>
2025-04-13 23:42:37 +0300

    Completed

--- setroles.sql
+++ setroles.sql
@@ -1,6 +1,6 @@
 -- Create admin user with full access
 CREATE USER 'bookstore_admin'@'localhost' IDENTIFIED BY 'admin_password';
-GRANT ALL PRIVILEGES ON bookstore.* TO 'bookstore_admin'@'localhost';
+GRANT ALL ON *.* TO 'bookstore_admin'@'localhost';
 -- Create staff user with limited access
 CREATE USER 'bookstore_staff'@'localhost' IDENTIFIED BY 'staff_password';
 GRANT SELECT, INSERT, UPDATE ON bookstore.* TO 'bookstore_staff'@'localhost';
